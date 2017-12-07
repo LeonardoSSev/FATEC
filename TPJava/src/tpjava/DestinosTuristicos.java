ac5e3c4cbed8c5f461d695c1ebc4dfcf3046459a
Leonardo Santos Severino <lseverino@mgovbrasil.com.br>
2017-12-07 16:16:13 -0200

    Start Projeto_java_destinos_turisticos

---
+++ TPJava/src/tpjava/DestinosTuristicos.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpjava;
+
+/**
+ *
+ * @author leo_a
+ */
+public class DestinosTuristicos {
+    private String nome;
+    private String cidade;
+    private String estado;
+    private String historia;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+    
+}
