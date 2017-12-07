e5de5d580cc68ff2424189228e27466d5eefaeb4
Leonardo Santos Severino <lseverino@mgovbrasil.com.br>
2017-12-07 16:50:36 -0200

    Implementing switch case and methods statements

--- TPJava/src/tpjava/TPJava.java
+++ TPJava/src/tpjava/TPJava.java
@@ -25,10 +25,37 @@ public class TPJava {
         
         do{
             op = JOptionPane.showOptionDialog(null, "Bem vindo!\\nInforme uma opção através dos botões abaixo:", "Destinos Turísticos", JOptionPane.PLAIN_MESSAGE, JOptionPane.QUESTION_MESSAGE, null, buttons, buttons[4]);
+            switch(op){
+                case 0:
+                    cadastrarDestino();
+                    break;
+                case 1:
+                    listarDestinos();
+                    break;
+                case 2:
+                    alterarDestino();
+                    break;
+                case 3:
+                    excluirDestino();
+                    break;
+            }
         }while(op != 4);
          
-        
         JOptionPane.showMessageDialog(null, "A aplicação foi encerrada.", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
     }
+    
+    public static void cadastrarDestino(){
+    
+    }
+    public static void listarDestinos(){
+    
+    }
+    public static void alterarDestino(){
+    
+    }
+    public static void excluirDestino(){
+    
+    }
+    
     
 }
