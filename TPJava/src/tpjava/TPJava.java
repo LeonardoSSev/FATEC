a7d32695ca1e5284a7e633a47f1dd84a88275ea2
Leonardo Santos Severino <lseverino@mgovbrasil.com.br>
2017-12-07 16:46:54 -0200

    Implementing menu options.

--- TPJava/src/tpjava/TPJava.java
+++ TPJava/src/tpjava/TPJava.java
@@ -5,6 +5,11 @@
  */
 package tpjava;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author leo_a
@@ -16,6 +21,14 @@ public class TPJava {
      */
     public static void main(String[] args) {
         int op;
+        String[] buttons = { "Cadastrar", "Listar", "Alterar", "Excluir", "Sair"};
+        
+        do{
+            op = JOptionPane.showOptionDialog(null, "Bem vindo!\\nInforme uma opção através dos botões abaixo:", "Destinos Turísticos", JOptionPane.PLAIN_MESSAGE, JOptionPane.QUESTION_MESSAGE, null, buttons, buttons[4]);
+        }while(op != 4);
+         
+        
+        JOptionPane.showMessageDialog(null, "A aplicação foi encerrada.", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
     }
     
 }
