7826e148dcc160c28715720a9a04e59fa2faf319
Leonardo Santos Severino <lseverino@mgovbrasil.com.br>
2017-12-08 01:52:21 -0200

    Implementing cadastrarDestino and listarDestinos methods.

--- TPJava/src/tpjava/TPJava.java
+++ TPJava/src/tpjava/TPJava.java
@@ -5,10 +5,10 @@
  */
 package tpjava;
 
-import javax.swing.JDialog;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+
 
 /**
  *
@@ -26,9 +26,9 @@ public class TPJava {
             exibeMenu(op, opcoes);
         }catch(Exception ex){
             JOptionPane.showMessageDialog(null, "Erro na aplicação\n" + ex, "Erro - Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
-        }finally{
-            JOptionPane.showMessageDialog(null, "A aplicação foi encerrada.", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
-        }        
+        }
+		
+        JOptionPane.showMessageDialog(null, "A aplicação foi encerrada.", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);        
     }
     
     public static void exibeMenu(int op, String buttons[]){
@@ -39,30 +39,72 @@ public class TPJava {
                          cadastrarDestino();
                          break;
                     case 1:
-                         listarDestinos();
+                        try {
+                            listarDestinos();
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, "Erro:\n " + e, "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+                        }
                          break;
                     case 2:
-                         alterarDestino();
+                         try {
+                            alterarDestino();
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, "Erro:\n " + e, "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+                        }
                          break;
                     case 3:
-                        excluirDestino();
-                        break;
+                        try {
+                            excluirDestino();
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, "Cadastro Erro:\n " + e, "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+                        }
+                         break;
                  }
          }while(op != 4);
     }
     
     public static void cadastrarDestino(){
-        JOptionPane.showMessageDialog(null, "Cadastrar Destino", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
-    }
-    public static void listarDestinos(){
-        JOptionPane.showMessageDialog(null, "Listar Destinos", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
-    }
-    public static void alterarDestino(){
-        JOptionPane.showMessageDialog(null, "Alterar Destino", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
-    }
-    public static void excluirDestino(){
-        JOptionPane.showMessageDialog(null, "Excluir Destino", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+        try{
+            String nome = JOptionPane.showInputDialog(null, "Informe o nome do novo destino turístico no campo abaixo:", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+            String cidade = JOptionPane.showInputDialog(null, "Informe, no campo abaixo, a cidade em que " + nome + " está localizado(a):", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+            String estado = JOptionPane.showInputDialog(null, "Informe, no campo abaixo, o estado em que " + nome + " está localizado(a):", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+            String historia = JOptionPane.showInputDialog(null, "Informe, no campo abaixo, a história em que " + nome + " está localizado(a):", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+            DestinoTuristico.destinosTuristicos.add(new DestinoTuristico(nome, cidade, estado, historia));
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Cadastro Erro:\n " + e, "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            JOptionPane.showMessageDialog(null, "Destino Turístico cadastrado com sucesso!", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+        }        
     }
     
-    
+    public static void listarDestinos() throws FileNotFoundException, IOException{
+        String conteudo = "";
+        try{    
+            for (DestinoTuristico dt : DestinoTuristico.destinosTuristicos) {
+                conteudo+=
+                        "Nome: "+ dt.getNome() + 
+                        "\nCidade: " + dt.getCidade() + 
+                        "\nEstado: " + dt.getEstado() +
+                        "\nHistória: " + dt.getHistoria()+ 
+                        "\n\n\n";
+            }
+            if(conteudo.equals("")){
+                JOptionPane.showMessageDialog(null, "Não há dados cadastrados ainda.", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);  
+            }else{
+                JOptionPane.showMessageDialog(null, conteudo, "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+            }
+            
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "Listagem Erro:\n " + ex, "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private static void alterarDestino() {
+        
+    }
+
+    private static void excluirDestino() {
+        
+    }
 }
+    
