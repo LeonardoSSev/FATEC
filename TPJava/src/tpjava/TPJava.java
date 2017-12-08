13712cf6f4e0d1dcf4682345ec076fdf79fbfc60
Leonardo Santos Severino <lseverino@mgovbrasil.com.br>
2017-12-08 03:15:40 -0200

    Upgrading the all the code

--- TPJava/src/tpjava/TPJava.java
+++ TPJava/src/tpjava/TPJava.java
@@ -50,17 +50,26 @@ public class TPJava {
     }
     
     public static void cadastrarDestino(){
+        boolean flag = false;
+        String metodo = "cadastrado";
         try{
             String nome = JOptionPane.showInputDialog(null, "Informe o nome do novo destino turístico no campo abaixo:", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
             String cidade = JOptionPane.showInputDialog(null, "Informe, no campo abaixo, a cidade em que " + nome + " está localizado(a):", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
             String estado = JOptionPane.showInputDialog(null, "Informe, no campo abaixo, o estado em que " + nome + " está localizado(a):", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
             String historia = JOptionPane.showInputDialog(null, "Informe, no campo abaixo, a história de " + nome + ":", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
-            DestinoTuristico.destinosTuristicos.add(new DestinoTuristico(nome, cidade, estado, historia));
+            
+            if(nome.isEmpty() || cidade.isEmpty() || estado.isEmpty() || historia.isEmpty()){
+                JOptionPane.showMessageDialog(null, "Dados não aceitos para cadastro!", "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+            }else{
+                DestinoTuristico.destinosTuristicos.add(new DestinoTuristico(nome, cidade, estado, historia));
+                JOptionPane.showMessageDialog(null, "Destino Turístico cadastrado com sucesso!", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+                flag = true;
+            }
+            verificaFlag(flag, metodo);
+            
         }catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Cadastro Erro:\n " + e, "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
-        }finally{
-            JOptionPane.showMessageDialog(null, "Destino Turístico cadastrado com sucesso!", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
-        }        
+            getMensagemErro("Cadastrar", e);
+        }
     }
     
     public static String listarDestinos(){
@@ -74,20 +83,22 @@ public class TPJava {
                         "\nHistória: " + dt.getHistoria()+ 
                         "\n\n\n";
             }
-            if(conteudo.equals("")){
+            if(conteudo.isEmpty()){
                 JOptionPane.showMessageDialog(null, "Não há dados cadastrados ainda.", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);  
             }else{
                 JOptionPane.showMessageDialog(null, conteudo, "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
             }
             
-        }catch(Exception ex){
-            JOptionPane.showMessageDialog(null, "Listagem Erro:\n " + ex, "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+        }catch(Exception e){
+            getMensagemErro("Listar", e);
         }
+        
         return conteudo;
     }
 
     private static void alterarDestino() {
         String listagem = listarDestinos();
+        String metodo = "alterado";
         
         if(!listagem.isEmpty()){
             String conteudo = "";
@@ -116,16 +127,49 @@ public class TPJava {
                 if(flag){
                     JOptionPane.showMessageDialog(null, "Destino alterado com sucesso!", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
                 } else{
-                    JOptionPane.showMessageDialog(null, "Dados não encontrados.", "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+                    getMensagemFalha(metodo);
                 }
             }catch(Exception e){
-                JOptionPane.showMessageDialog(null, "Alterar Erro:\n " + e, "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+                getMensagemErro("Alterar", e);
             }
+            verificaFlag(flag, metodo);
         }
 
     }
 
     private static void excluirDestino() {
+        String listagem = listarDestinos();
+        String metodo = "excluído";
+        String conteudo = "";
+        boolean flag = false;
+        if(!listagem.isEmpty()){
+            String[] dados = buscarDestino();
+            try{
+                for (int i = 0; i < DestinoTuristico.destinosTuristicos.size(); i++) {
+                    if((DestinoTuristico.destinosTuristicos.get(i).getNome().equalsIgnoreCase(dados[0])) && (DestinoTuristico.destinosTuristicos.get(i).getCidade().equalsIgnoreCase(dados[1]))){
+                        DestinoTuristico dt = DestinoTuristico.destinosTuristicos.get(i);
+                        conteudo+=
+                            "Nome: "+ dt.getNome() + 
+                            "\nCidade: " + dt.getCidade() + 
+                            "\nEstado: " + dt.getEstado() +
+                            "\nHistória: " + dt.getHistoria()+ 
+                            "\n\n\n";
+
+                        int excluir = JOptionPane.showConfirmDialog (null, conteudo, "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+                        if(excluir == JOptionPane.YES_OPTION){
+                            DestinoTuristico.destinosTuristicos.remove(i);
+                            flag = true;
+                            JOptionPane.showMessageDialog(null, "Destino excluído com sucesso!", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+                        }
+                    }
+                }
+                
+                
+            }catch(Exception e){
+                getMensagemErro("Excluir", e);
+            }
+            verificaFlag(flag, metodo);
+        }
         
     }
     
@@ -134,6 +178,24 @@ public class TPJava {
         dados[0] = JOptionPane.showInputDialog(null, "Informe, no campo abaixo, o nome do destino que você deseja alterar:", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
         dados[1] = JOptionPane.showInputDialog(null, "Informe, no campo abaixo, a cidade do destino que você deseja alterar:", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
         return dados;
+    }
+    
+    private static void verificaFlag(boolean flag, String metodo){
+        if(flag){
+                getMensagemSucesso(metodo);
+            } else{
+                getMensagemFalha(metodo);
+            }
+    }
+    
+    private static void getMensagemSucesso(String metodo){
+        JOptionPane.showMessageDialog(null, "Destino " + metodo + "  com sucesso!", "Destinos Turísticos", JOptionPane.INFORMATION_MESSAGE);
+    }
+    private static void getMensagemFalha(String metodo){
+        JOptionPane.showMessageDialog(null, "Destino não " + metodo +"!", "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
+    }
+    private static void getMensagemErro(String metodo, Exception e){
+         JOptionPane.showMessageDialog(null, "Erro ao " + metodo + ":\n " + e, "Destinos Turísticos", JOptionPane.ERROR_MESSAGE);
     }
 }
     
